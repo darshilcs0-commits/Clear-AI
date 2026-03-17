44f2945b97eecd04c1788eeee38279275484d1a6
darshilcs0-commits <darshilcs0@gmail.com>
2026-03-17 13:15:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def chat():
         6. The replies should be SHORT but the content should be fire.
         7. Don't be over abusive but be a great roaster, also appritiate them if they use logic somewhere and never use sexual words like "hilana" "muth marna" because the other one can be a girl also!
         
-        Make them laugh, but absolutely destroy their self-esteem in natural, street-level Hinglish."""
+        Make them laugh, don't become too rude destroy their self-esteem in natural, street-level Hinglish."""
         
         # MEMORY LOGIC START: System prompt yahan pass kiya taaki history me kachra na bhare
         model = genai.GenerativeModel('gemini-2.5-flash', system_instruction=system_prompt)
